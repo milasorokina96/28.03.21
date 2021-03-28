5ad046fb0711765cc20d178733472485ec4be7fe
Lyuda.Pik <milasorokina96@gmail.com>
2021-03-28 16:41:46 +0300

    comment

--- cafe/assets/script.js
+++ cafe/assets/script.js
@@ -34,41 +34,49 @@ const menu = {
 
 
 const order = async (food) => {
+  // Тут мы готовим каждое блюдо
     const cookFood = ({ name, time }) =>
+    // Для этого создаем промис, который выполняется успешно или не в зависимости от условия
       new Promise((resolve, reject) => {
         const condition = +Math.random().toFixed();
         setTimeout(() => {
           condition ? resolve(name) : reject(name);
         }, time);
       });
-  
+
+    // Тут мы ожидаем окончания приготовления всех блюд
+    // allSattled возвращает объект со статусом fulfilled или rejected и значением
     const completeOrder = await Promise.allSettled(food.map(cookFood));
-  
+
+    // Тут проверяем, что все блюда готовы
     if (completeOrder.every((x) => x.status === "fulfilled")) {
       return completeOrder;
     } else {
       throw new Error("Something went wrong...1");
     }
   };
-  
-  const checkOrders = async (orders) => {
-    const completeOrders = await Promise.allSettled(orders);
-    const ordersStatistic = completeOrders.reduce((a, x) => {
-      a[x.status] || (a[x.status] = []);
-      a[x.status].push(x);
-      return a;
-    }, {});
-  
-    if (ordersStatistic.fulfilled?.length > 2) {
-      return ordersStatistic.fulfilled.map((order) =>
-        order.value.map((x) => x.value)
-      );
-    } else {
-      throw new Error("Something went wrong...2");
-    }
-  };
-  
-  checkOrders([order(menu.burger), order(menu.hotDog), order(menu.pizza)])
-    .then(console.log)
-    .catch((e) => console.log(e.message));
 
+    const checkOrders = async (orders) => {
+      // Тут мы ждем все заказы и ожидаем их выполнения
+      const completeOrders = await Promise.allSettled(orders);
+    
+      // Создаем объект, чтоб сразу разделить выполненные и нет
+      const ordersStatistic = completeOrders.reduce((a, x) => {
+        a[x.status] || (a[x.status] = []);
+        a[x.status].push(x);
+        return a;
+      }, {});
+    
+      // Проверяем сколько выполненно. Если нужны все, то выше reduce заменяем на every
+      if (ordersStatistic.fulfilled?.length > 2) {
+        return ordersStatistic.fulfilled.map((order) =>
+          order.value.map((x) => x.value)
+        );
+      } else {
+        throw new Error("Something went wrong...");
+      }
+    };
+    
+    checkOrders([order(menu.burger), order(menu.hotDog), order(menu.pizza)])
+      .then(console.log)
+      .catch((e) => console.log(e.message));
