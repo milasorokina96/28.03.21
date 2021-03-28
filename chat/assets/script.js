4d32c84a4856653ea18fa160597c971cd2bd4e14
Lyuda.Pik <milasorokina96@gmail.com>
2021-03-28 02:37:47 +0200

    chat

--- chat/assets/script.js
+++ chat/assets/script.js
@@ -4,68 +4,77 @@
 // Пользователь может в любой момент прекратить диалог написав сообщение, например, 'Bye'. 
 // Тогда браузер должен вежливо попрощаться.
 
+class Chat {
+  constructor(btnSend, userSms, chatArea) {
+    this.btnSend = btnSend,
+    this.userSms = userSms,
+    this.chatArea = chatArea,
 
-function timeout(sms, time) {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(sms), time * 1000);
-  });
-
-}
-
-function rand(min, max) {
-  return Math.floor(Math.random() * max + min);
-
-}
-
-function createBrowserSms(userSms){
-  let browserSms;
-  if(userSms === 'Bye' || userSms === 'bye') {
-    browserSms = 'Bye';
-  } else {
-    browserSms = [
-      'Hello! I am a unicorn',
-      'Let dance',
-      'I can answer random sms',
-      'Okey',
-      'I am fine, and you?',
-    ][rand(0, 4)];
+    this.sendSms();
   }
-  return timeout(browserSms, rand(1, 10));
-}
 
-async function showBrowserAnswer(userSms) {
+  timeout(sms, time) {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(sms), time * 1000);
+    });
   
-  let browserSms = await createBrowserSms(userSms); // дождаться выполнения Таймера
-
-  createSms(browserSms, 'red');
-
-}
-
-function createSms(text, color){
-  const chatArea = document.getElementById('chatArea');
-  const sms = document.createElement('div');
-  sms.innerText = text;
-  sms.style.color = color;
-  chatArea.appendChild(sms);
-
-}
-
-function sendSms() {
-  const btnSend = document.getElementById('btnSend');
-  btnSend.addEventListener('click', () => {
-    const form = document.forms.chatForm;
-    const userSms = form.elements.chatSms;
-
-    if(userSms.value){
-      createSms(userSms.value, 'blue');
-      showBrowserAnswer(userSms.value);
-
+  }
+  
+  rand(min, max) {
+    return Math.floor(Math.random() * max + min);
+  
+  }
+  
+  createBrowserSms(userSms){
+    let browserSms;
+    if(userSms === 'Bye' || userSms === 'bye') {
+      browserSms = 'Bye';
+    } else {
+      browserSms = [
+        'Hello! I am a unicorn',
+        'Let dance',
+        'I can answer random sms',
+        'Okey',
+        'I am fine, and you?',
+      ][this.rand(0, 4)];
     }
+    return this.timeout(browserSms, this.rand(1, 10));
+  }
+  
+  async showBrowserAnswer(userSms) {
+    
+    let browserSms = await this.createBrowserSms(userSms); // дождаться выполнения Таймера
+  
+    this.createSms(browserSms, 'red');
+  
+  }
 
-    userSms.value='';
+  createSms(text, color){
+    const sms = document.createElement('div');
+    sms.innerText = text;
+    sms.style.color = color;
+    this.chatArea.appendChild(sms);
+  
+  }
 
-  });
+  sendSms() {
+  
+    this.btnSend.addEventListener('click', () => {
+  
+      if(this.userSms.value){
+        this.createSms(this.userSms.value, 'blue');
+        this.showBrowserAnswer(this.userSms.value);
+  
+      }
+      this.userSms.value = '';
+  
+    });
+  }
 }
 
+new Chat (
+  document.getElementById('btnSend'),
+  document.forms.chatForm.chatSms,
+  document.getElementById('chatArea'),
 
-sendSms();
+)
