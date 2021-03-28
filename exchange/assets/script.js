bcf6faf80966490024c10a5c2fea46574ca632a8
Lyuda.Pik <milasorokina96@gmail.com>
2021-03-28 02:37:12 +0200

    exchange2

--- exchange/assets/script.js
+++ exchange/assets/script.js
@@ -1,32 +1,49 @@
 // Создать небольшой обмен валют. Перевести гривну в доллар, евро, рубль.
-// url https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json
+// url: 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
 
+class Exchange {
+  constructor(url, btnExchange, uah, select, result) {
+    this.url = url,
+    this.btnExchange = btnExchange;
+    this.uah = uah;
+    this.select = select;
+    this.result = result;
 
-fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    getMoney(data);
+    this.getMoney();
+  }
+
+  async fetchData(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  }
+
+  getMoney() {
+     
+    this.btnExchange.addEventListener('click', async () => {
+      const data = await this.fetchData(this.url);
     
-  });
+      if (this.uah.value){
 
+        data.forEach(function(item){
 
-function getMoney(data) {
-  const btnExchange = document.getElementById('btnExchange');
-  const form = document.forms.formExchange;
-  const uah = form.elements.UAH;
-  const select = form.elements.exchange;
-  const result = document.getElementById('exchangeResult');
-
-  btnExchange.addEventListener('click', () => {
-    data.forEach(function(item){
-      if(item.cc === select.value){
-        result.innerText = (uah.value / item.rate).toFixed(2) + ' ' + select.value;
+          if(item.cc === this.select.value){
+            
+            this.result.innerText = (this.uah.value / item.rate).toFixed(2) + ' ' + this.select.value;
+          }
+    
+        });
       }
-
+  
     });
-    
-
-  });
+  }
 }
+
+new Exchange(
+  'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
+  document.getElementById('btnExchange'),
+  document.forms.formExchange.elements.UAH,
+  document.forms.formExchange.elements.exchange,
+  document.getElementById('exchangeResult'),
+);
